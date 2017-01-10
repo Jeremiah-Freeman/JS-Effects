95ef655386041158139ab1e082728bd49684594e
Jeremiah Freeman <jaythinkshappiness@gmail.com>
2017-01-10 11:41:35 -0800

    delete all mis-created folder, update and work further on Peek-a-boo.html

---
+++ Peek-a-boo/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#buddha-showing").fadeToggle();
+    $("#buddha-hidden").fadeToggle() ;
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#buddha").fadeToggle();
+    $("#buddha").fadeToggle() ;
+  });
+});
